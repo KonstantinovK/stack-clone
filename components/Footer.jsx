7b63c6ccd6b1ef32d4f22aec51a542218b3981b4
[KonstantinovK] <[konstantinov87@mail.ru]>
2026-03-03 15:18:53 +0300

    fix footer address

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -23,10 +23,10 @@ export default function Footer() {
                   <span className="footer-block-text">Email: hello@stackroom.com</span>
                 </li>
                 <li className="footer-block-item">
-                  <span className="footer-block-text">Телефон: +7 (999) 123-45-67</span>
+                  <span className="footer-block-text">Телефон: +7 (903) 612-73-66</span>
                 </li>
                 <li className="footer-block-item">
-                  <span className="footer-block-text">Адрес: Москва, ул. Примерная, 123</span>
+                  <span className="footer-block-text">Адрес: Московская область, г.о. Химки, ул.Соколовуская 4 квартал, д.18</span>
                 </li>
               </ul>
             </div>
